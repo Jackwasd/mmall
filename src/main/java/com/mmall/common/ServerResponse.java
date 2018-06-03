d8b9de72bb02a81b3226e849b71d7f035298a4aa
Jack <1291986260@qq.com>
2018-06-03 14:34:44 +0800

    second

---
+++ src/main/java/com/mmall/common/ServerResponse.java
@@ -0,0 +1,87 @@
+package com.mmall.common;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import java.io.Serializable;
+
+/**
+ * 通用的数据端响应对象
+ * @param <T>
+ */
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+//保证序列化JSON的时候，如果value为NULL的话,那么key也会消失
+public class ServerResponse<T> implements Serializable{
+    private int status;
+    private String msg;
+    private T data;
+
+    private ServerResponse(int status) {
+        this.status = status;
+    }
+
+    private ServerResponse(int status, T data){
+        this.status = status;
+        this.data = data;
+    }
+
+    private ServerResponse(int status, String msg, T data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    private ServerResponse(int status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+
+    @JsonIgnore
+    //使之不在Json序列化的结果之中
+    public boolean isSuccess(){
+        return this.status == ResponseCode.SUCCESS.getCode();
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    //创建成功的话，就将状态码赋值为0
+    public static <T> ServerResponse<T> createBySuccess(){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode());
+    }
+
+    //创建成功的话,将状态码赋值为0以及将成功信息返回
+    public static <T> ServerResponse<T> createBySuccessMessage(String msg){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), msg);
+    }
+
+    //创建成功的话将状态码赋值为0,并且将数据也返回
+    public static <T> ServerResponse<T> createBySuccess(T data){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), data);
+    }
+
+    //创建成功的话将状态码赋值为0,同时将信息以及数据均返回
+    public static <T> ServerResponse<T> createBySuccess(String msg, T data){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), msg, data);
+    }
+
+    //下面三个是创建失败的时候的情况
+    public static <T> ServerResponse<T> createByError(){
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(), ResponseCode.ERROR.getDesc());
+    }
+    public static <T> ServerResponse<T> createByErrorMessage(String errorMessage){
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(), errorMessage);
+    }
+    public static <T> ServerResponse<T> createByErrorCodeMessage(int errorCode, String errorMessage){
+        return new ServerResponse<T>(errorCode, errorMessage);
+    }
+}
